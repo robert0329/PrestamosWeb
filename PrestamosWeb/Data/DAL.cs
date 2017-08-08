8acdec89fd0485f1d35ec420dec9274ee81aa054
robert antonio frias de leon <chico_3_13@hotmail.com>
2017-08-07 20:55:19 -0400

    Vamos al mambo

---
+++ PrestamosWeb/Data/DAL.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.Configuration;
+
+namespace PrestamosWeb.Data
+{
+    public class DAL : DbContext
+    {
+        public DAL() : base()
+        {
+
+        }
+    }
+}
---
+++ PrestamosWeb/Models/Usuarios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PrestamosWeb.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+
+        [Required(ErrorMessage = "Primer Nombre es requerido.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Apellido es Requerido.")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "Email es Requerido")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Usuario es Requerido")]
+        public string NombreUsuario { get; set; }
+
+        [Required(ErrorMessage = "Password es requerido")]
+        [DataType(DataType.Password)]
+        public string Contraseña { get; set; }
+
+        [Compare("Contraseña", ErrorMessage = "No coinciden las contraseñas")]
+        [DataType(DataType.Password)]
+        public string ConfirmContraseña { get; set; }
+    }
+}
